3592388d7f4a3f62973e3c3553854f3951387151
Tyron <tyronodame5@gmail.com>
2024-10-02 15:04:25 -0400

    question 2

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 //Code your solutions in this file
+const multiplesOfThree = () => {
+    let threes = [];
+
+    for (let i = 1; i <= 100; i++) {
+        if (i % 3 == 0) {
+            threes.push(i)
+        }
+    }
+    return threes
+}
+
+console.log(multiplesOfThree());
